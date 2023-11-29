065cd5b0347e71cba2c32270f090c6957dd04c9a
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2023-11-29 03:24:07 -0700

    Included View all, 1 and updated to post data

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -1,6 +1,53 @@
 const router = require('express').Router();
+// added sequelize
+const { json } = require('sequelize');
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
+// add put and view all and 1 from Post 
+// here
+// The `/api/post` endpoint
+
+// GET all post
+router.get('/', async (req, res) => {
+  // find all post
+  try {
+    const postData = await Post.findAll(
+      {
+        include: {
+          model: Comment
+        }
+      }
+    );
+    res.status(200).json(posttData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // included its associated Comment data
+});
+
+// GET one post
+router.get('/:id', async (req, res) => {
+  // find a single post by its `id`
+  try {
+    const postData = await Post.findOne(
+      { where:{
+        id: req.params.id
+      },
+      include:  {
+        model: Comment
+      }
+      }
+    );
+    if (!postData) {
+      return res.status(404).json({message: 'no Post found for this id'})
+    }
+    res.status(200).json(postData)
+  } catch(err) {
+    res.status(500).json(err);
+  }
+  // included its associated Comment data
+});
+// there
 
 router.post('/', withAuth, async (req, res) => {
   try {
