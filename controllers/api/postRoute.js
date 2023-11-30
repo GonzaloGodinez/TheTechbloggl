3166203fa32530181ebc4c5a45b3b0b826d50b2e
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2023-11-30 00:22:25 -0700

    update postRoute server.js files

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
         }
       }
     );
-    res.status(200).json(posttData);
+    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
