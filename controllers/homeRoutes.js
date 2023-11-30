db0d9be7c7fa92f79569fa81716ce3c24745af08
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2023-11-30 00:23:34 -0700

    Update homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,11 +16,12 @@ router.get('/', async (req, res) => {
 
     // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
-
+    console.log(postData)
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      posts, 
-      logged_in: req.session.logged_in 
+      layout: 'main', 
+      // logged_in: req.session.logged_in 
+      posts
     });
   } catch (err) {
     res.status(500).json(err);
@@ -40,7 +41,7 @@ router.get('/post/:id', async (req, res) => {
 
     const post = postData.get({ plain: true });
 
-    res.render('post', {
+    res.render('homepage', {
       ...post,
       logged_in: req.session.logged_in
     });
