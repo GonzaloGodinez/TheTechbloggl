40ba55321c0c218b8245a1ffc782a997534d0798
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2023-12-03 02:07:22 -0700

    added update functionality

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,8 +3,7 @@ const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
-const postData = require('./commentData.json');
-
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
